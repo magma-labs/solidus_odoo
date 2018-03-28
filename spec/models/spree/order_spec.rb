d27e21f3fd9561fc9c5da58ef866b821851c645f
Alberto Mendoza <albertm_805@outlook.com>
2018-03-27 21:51:02 -0600

    add order state test

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe Spree::Order do
-  let!(:order) { create(:order_with_line_items) }
+  let!(:order) { create(:order_with_line_items, state: 'complete') }
 
   context 'An order should be created' do
     it 'should be generated' do
-      binding.pry
       expect(Spree::Order.count).to be(1)
+    end
+
+    it 'should have status as completed' do
+      expect("complete").to eq(order.state)
     end
   end
 end
