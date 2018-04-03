8bd74970dacc1445a1e61753b544b218c0e905c8
Alberto Mendoza <albertm_805@outlook.com>
2018-04-03 16:39:48 -0500

    Minor fixes with tests - Fix rubocop comments 'line breaks' - Add tests for order line - Add tests for partner children

--- spec/models/odoo/partner_spec.rb
+++ spec/models/odoo/partner_spec.rb
@@ -26,5 +26,29 @@ describe Odoo::Partner do
       order.email = 'test@example.com'
       expect(ResPartner.find(email: order.email)).to be_truthy
     end
+
+    it 'should have an account with billing/shipping address' do
+      address = order.ship_address
+
+      def partner_attributes(address)
+        {
+          name: order.name,
+          street: address.address1,
+          city: address.city,
+          state_id: address.id,
+          zip: address.zipcode,
+          country_id: address.id,
+          phone: address.phone,
+          email: order.email
+        }
+      end
+
+      @odoo_partner = ResPartner.create partner_attributes(address)
+      @invoice_partner = ResPartner.create partner_attributes(order.bill_address).merge(name: "billing", type: "invoice", parent_id: @odoo_partner.id)
+      @delivery_partner = ResPartner.create partner_attributes(order.ship_address).merge(name: "shipping", type: "delivery", parent_id: @odoo_partner.id)
+      @odoo_partner.save
+
+      expect(@odoo_partner.associations['child_ids']).not_to be_blank
+    end
   end
 end
