519f7ec1bd51d8f98b3769f6381fae0132ec343d
Alberto Mendoza <albertm_805@outlook.com>
2018-04-06 09:59:46 -0500

    WIP

--- spec/models/odoo/order_line_spec.rb
+++ spec/models/odoo/order_line_spec.rb
@@ -1,26 +1,40 @@
 require 'spec_helper'
 
 describe Odoo::OrderLine do
-  let!(:order) { create(:order_with_line_items, state: 'complete') }
-  let!(:product) { create(:base_product, name: 'Samsung Tv', slug: 'samsung-tv') }
+  let!(:shipping_option) { create(:shipping_method, cost: 0) }
+  let(:order) { create(:order_with_line_items, state: 'complete', line_items: [create(:line_item, product: product)]) }
+  let(:product) { create(:product, shipping_category: shipping_category) }
+  let(:shipping_category) { create(:shipping_category, shipping_methods: [shipping_method]) }
+  let(:shipping_method) { create(:shipping_method) }
+  Ooor.new(url: 'https://magmalabs1.odoo.com', database: 'magmalabs1', username: 'alberto.mendoza@magmalabs.io', password: 'Please123')
 
   context 'When a spree order is sent to odoo' do
+    before :each do
+      binding.pry
+      calculator = product.shipping_category.shipping_methods.first.calculator
+      calculator.preferred_amount = "0.0"
+      calculator.save
+    end
     it 'should throw "name" error if product does not exist in odoo' do
       line_item = order.products.last
       expect(ProductProduct.find(default_code: line_item.slug)).to be_blank
     end
 
     it 'should find product in odoo' do
-      expect(ProductProduct.find(default_code: product.slug)).to be_truthy
+      expect(ProductProduct.find(default_code: 'product-3-935')).to be_truthy
     end
 
     it 'should complete an order' do
+      binding.pry
       product = order.products.last
       product.name = "Samsung Tv"
+      product.save!
       product.slug = "samsung-tv"
       product.save!
       product.reload
       order.reload
+      OdooService.create_order(order)
+      odoo_order = SaleOrder.find(['name', '=', order.number])
     end
   end
 end
