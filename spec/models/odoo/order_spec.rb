d9f92fbb7a11cd8cf377d67cc6a446e1d7ba4a97
Alberto Mendoza <albertm_805@outlook.com>
2018-04-11 10:49:11 -0500

    add order spec

---
+++ spec/models/odoo/order_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Odoo::Order do
+  let(:order) { create(:order_with_line_items, state: 'complete') }
+
+  context 'When a spree order is sent to odoo' do
+
+    it 'should have total sale price in odoo as in solidus' do
+      product = order.products.last
+      product.name = "Samsung Tv"
+      product.save!
+      product.slug = "samsung-tv"
+      product.save!
+      product.reload
+      order.reload
+      binding.pry
+      OdooService.create_order(order)
+
+      expect(SaleOrder.last.amount_total).to eq(order.total - order.ship_total)
+    end
+  end
+end
