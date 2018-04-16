12644588bbe0c8a9075cb16ef30eb6cd37d38436
Alberto Mendoza <albertm_805@outlook.com>
2018-04-16 18:38:43 -0500

    refactor country and order spec

--- spec/models/odoo/country_spec.rb
+++ spec/models/odoo/country_spec.rb
@@ -4,11 +4,12 @@ describe Odoo::Country do
   let!(:order) { create(:order_with_line_items, state: 'complete') }
 
   context 'when an order is completed in solidus and Sale Order line is created in Odoo' do
+    before(:each) do
+      @address = order.ship_address.country.name
+      @country = ResCountry.find(['name', '=', @address]).first.name
+    end
     it 'should have a country' do
-      address = order.ship_address.country.name
-      country = ResCountry.find(['name', '=', address]).first.name
-
-      expect('United States').to eq(country)
+      expect('United States').to eq(@country)
     end
   end
 end
--- spec/models/odoo/order_spec.rb
+++ spec/models/odoo/order_spec.rb
@@ -4,15 +4,17 @@ describe Odoo::Order do
   let(:order) { create(:order_with_line_items, state: 'complete') }
 
   context 'When a spree order is sent to odoo' do
+    before(:each) do
+      @product = order.products.last
+      @product.name = "Samsung Tv"
+      @product.save!
+      @product.slug = "samsung-tv"
+      @product.save!
+      @product.reload
+      order.reload
+    end
 
     it 'should have total sale price in odoo as in solidus' do
-      product = order.products.last
-      product.name = "Samsung Tv"
-      product.save!
-      product.slug = "samsung-tv"
-      product.save!
-      product.reload
-      order.reload
       OdooService.create_order(order)
 
       expect(SaleOrder.last.amount_total).to eq(order.total - order.ship_total)
