8761c0baa525fd92e309422c53514a38a8052ef5
Alberto Mendoza <albertm_805@outlook.com>
2018-04-16 16:49:01 -0500

    refactor and add tests

--- spec/models/odoo/country_spec.rb
+++ spec/models/odoo/country_spec.rb
@@ -4,10 +4,10 @@ describe Odoo::Country do
   let!(:order) { create(:order_with_line_items, state: 'complete') }
 
   context 'when an order is completed in solidus and Sale Order line is created in Odoo' do
-
     it 'should have a country' do
       address = order.ship_address.country.name
       country = ResCountry.find(['name', '=', address]).first.name
+
       expect('United States').to eq(country)
     end
   end
--- spec/models/odoo/order_line_spec.rb
+++ spec/models/odoo/order_line_spec.rb
@@ -1,18 +1,9 @@
 require 'spec_helper'
 
 describe Odoo::OrderLine do
-  let!(:shipping_option) { create(:shipping_method, cost: 0) }
-  let(:order) { create(:order_with_line_items, state: 'complete', line_items: [create(:line_item, product: product)]) }
-  let(:product) { create(:product, shipping_category: shipping_category) }
-  let(:shipping_category) { create(:shipping_category, shipping_methods: [shipping_method]) }
-  let(:shipping_method) { create(:shipping_method) }
+  let(:order) { create(:order_with_line_items, state: 'complete') }
 
   context 'When a spree order is sent to odoo' do
-    before :each do
-      calculator = product.shipping_category.shipping_methods.first.calculator
-      calculator.preferred_amount = "0.0"
-      calculator.save
-    end
     it 'should throw "name" error if product does not exist in odoo' do
       line_item = order.products.last
       expect(ProductProduct.find(default_code: line_item.slug)).to be_blank
